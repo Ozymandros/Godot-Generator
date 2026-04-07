aab42e2a866a94f1db28a9421f7d6561dabe01b8
Andreu Martínez Vidal <andreumv@hotmail.com>
2026-04-07 18:45:08 +0200

    feat: Refactor navigation components to use AppNavLink and AppNavExternalLink for improved consistency and maintainability

---
+++ GodotGenerator.Blazor/GodotGenerator.Blazor/Components/Layout/AppIconFactory.cs
@@ -0,0 +1,24 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+using FluentIcons = Microsoft.FluentUI.AspNetCore.Components.Icons;
+
+namespace GodotGenerator.Blazor.Components.Layout;
+
+public static class AppIconFactory
+{
+    public static Icon Settings { get; } = new FluentIcons.Regular.Size20.Settings();
+    public static Icon Layer { get; } = new FluentIcons.Regular.Size20.Layer();
+    public static Icon Code { get; } = new FluentIcons.Regular.Size20.Code();
+    public static Icon Text { get; } = new FluentIcons.Regular.Size20.TextT();
+    public static Icon Image { get; } = new FluentIcons.Regular.Size20.Image();
+    public static Icon Grid { get; } = new FluentIcons.Regular.Size20.Grid();
+    public static Icon Play { get; } = new FluentIcons.Regular.Size20.Play();
+    public static Icon Speaker2 { get; } = new FluentIcons.Regular.Size20.Speaker2();
+    public static Icon Video { get; } = new FluentIcons.Regular.Size20.Video();
+    public static Icon AppsList { get; } = new FluentIcons.Regular.Size20.AppsList();
+    public static Icon Molecule { get; } = new FluentIcons.Regular.Size20.Molecule();
+    public static Icon FolderOpen { get; } = new FluentIcons.Regular.Size20.FolderOpen();
+    public static Icon Home { get; } = new FluentIcons.Regular.Size20.Home();
+    public static Icon DataArea { get; } = new FluentIcons.Regular.Size20.DataArea();
+    public static Icon DocumentBulletList { get; } = new FluentIcons.Regular.Size20.DocumentBulletList();
+    public static Icon BranchFork { get; } = new FluentIcons.Regular.Size20.BranchFork();
+}
